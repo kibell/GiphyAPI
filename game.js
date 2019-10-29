9f0ede4498de9b24086861086fb87bc0b278248e
Kitrick Bell <kitrickd.bell@gmail.com>
2019-10-29 17:12:51 -0500

    Added psuedo code to the .js file to help direct the project

---
+++ game.js
@@ -0,0 +1,35 @@
+// call the ajax function and set the varibles for the url 
+
+
+//create a div to hold the images
+
+
+//const the url image 
+
+
+
+//and the attribute to the images so that they are changed when they are called
+
+
+
+//prepend the images to a div
+
+
+
+//create a loop append a new button when the search button is selected
+
+
+//varibale for a new button
+
+//add a class to the new button
+
+//add a data attribute
+
+//label the new button that was created
+
+//append the button in the end
+
+
+
+//create a function to push the input to an array
+
