1b32f80c9641e3a6f49a3bcf74abae522c6a92f1
Kitrick Bell <kitrickd.bell@gmail.com>
2019-10-30 19:55:15 -0500

    fixed button class on the document function at the bottom of the JS file so that it would read the GIF appropiately

--- game.js
+++ game.js
@@ -1,33 +1,31 @@
 //create an array of choices
 
-const choices = ['Really Bro?', 'High five!','Way to go']
+const choices = ['Lord of the Rings', 'High five!','Way to go']
 
 
 // call the ajax function and set the varibles for the url 
-function getGif(){
+function getGif() {
 
-    const giphy = $(this).attr('data-name');
-    
-    const queryURL = `https://api.giphy.com/v1/gifs/random?api_key=do2zG5Nm1SN1Ioh0tMOY6mXWryN8X5yZ&tag=${giphy}&rating=PG-13`
-
-
+const ttt = $(this).attr("data-name");
+    const queryURL = `https://api.giphy.com/v1/gifs/random?api_key=do2zG5Nm1SN1Ioh0tMOY6mXWryN8X5yZ&tag=${ttt}&rating=PG-13`;
     $.ajax({
         url: queryURL,
-        method: 'GET'
+        method: "GET"
     }).then(function(response){
 
         console.log(response)
         
 //create a div to hold the images
-        const newDiv = $("<div class ='giphy'>")
+       
         //const the url image 
         const imageUrl = response.data.image_original_url
 
-        const newImage = $('<img>')
+        const newImage = $('<img>');
 //and the attribute to the images so that they are changed when they are called
 
 
         newImage.attr('src',imageUrl)
+        newImage.attr('alt', "new");
 //prepend the images to a div
 
 
@@ -46,37 +44,32 @@ function getGif(){
 function buttonLoop () {
 
     $('#addBtn').empty();
-    for ( let i =0; i < choices.length; i++){
+
+    for ( let i = 0; i < choices.length; i++){
 //varibale for a new button
-const btn = $('<button>');
+const a = $('<button>');
 //add a class to the new button
-btn.addClass('new-button')
+a.addClass("jiffy-btn");
 //add a data attribute
-btn.attr("data-name",choices[i])
+a.attr("data-name",choices[i]);
 //label the new button that was created
-btn.text(choices[i]);
+a.text(choices[i]);
 //appen("d the button in the end
-$('#addBtn').append(btn);
-}
+$('#addBtn').append(a);
+ }
 }
 //create a function to push the input to an array
 
-$('#myButton').on("click",function(event) {
+$('#add-gif').on("click", function(event) {
 event.preventDefault();
+
 const inputVal = $('#input').val().trim();
 
 choices.push(inputVal);
-
 buttonLoop();
-
-
-
-
-
-
 
 });
 
-$(document).on("click", "#addBtn", getGif);
+$(document).on("click", ".jiffy-btn", getGif);
 
 buttonLoop();
